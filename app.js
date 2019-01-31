a4954409a18dff77a47f6a49d4390b7195b523a7
verdantpath <m_dittmann@yahoo.com>
2019-01-30 19:07:19 -0800

    add routes

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('<h1>I love work!</h1>');
+});
+
+app.get('/hello', (req, res) => {
+  res.send('<h1>Hello, JavaScript Developer!</h1>');
+});
+
+app.listen(3000, () => {
+  console.log('The application is running on localhost:3000');
+});
