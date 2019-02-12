ba64ba9054756151df00e9381c1735812dce2f32
verdantpath <m_dittmann@yahoo.com>
2019-02-11 20:36:10 -0800

    cookie-parser, cookies for state

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 
 const app = express();
+
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 app.set('view engine', 'pug');
 
 app.get('/', (req, res) => {
-  res.render('index');
+  const name = req.cookies.username;
+  res.render('index', { name });
 });
 
 app.get('/cards', (req, res) => {
@@ -21,7 +25,8 @@ app.get('/hello', (req, res) => {
 });
 
 app.post('/hello', (req, res) => {
-  res.render('hello', { name: req.body.username });
+  res.cookie('username', req.body.username);
+  res.redirect('/');
 });
 
 app.listen(3000, () => {
