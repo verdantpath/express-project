33299be11b6916c73f9ec0159d757238e0867949
verdantpath <m_dittmann@yahoo.com>
2019-02-17 20:11:55 -0800

    error handling

--- app.js
+++ app.js
@@ -9,10 +9,12 @@ app.use(cookieParser());
 
 app.set('view engine', 'pug');
 
-app.use((req, res, next) => {
-  console.log('one two three');
-  next();
-});
+// app.use((req, res, next) => {
+//   console.log('one two three');
+//   const err = new Error('Uh oh');
+//   err.status = 500;
+//   next(err);
+// });
 
 app.get('/', (req, res) => {
   const name = req.cookies.username;
@@ -46,6 +48,18 @@ app.post('/hello', (req, res) => {
 app.post('/goodbye', (req, res) => {
   res.clearCookie('username');
   res.redirect('/hello');
+});
+
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  res.locals.error = err;
+  res.status(err.status);
+  res.render('error');
 });
 
 app.listen(3000, () => {
