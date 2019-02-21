4f561da1f1c277a980476257c8154e80361e0f5e
verdantpath <m_dittmann@yahoo.com>
2019-02-20 22:12:31 -0800

    move routes to index file

--- routes/index.js
+++ routes/index.js
@@ -1,2 +1,36 @@
 const express = require('express');
 const router = express.Router();
+
+app.get('/', (req, res) => {
+  const name = req.cookies.username;
+  if (name) {
+    res.render('index', { name });
+  } else {
+    res.redirect('/hello');
+  }
+});
+
+app.get('/cards', (req, res) => {
+  res.render('card', {
+    prompt: "Who is buried in Grant's tomb?"
+  });
+});
+
+app.get('/hello', (req, res) => {
+  const name = req.cookies.username;
+  if (name) {
+    res.redirect('/');
+  } else {
+    res.render('hello');
+  }
+});
+
+app.post('/hello', (req, res) => {
+  res.cookie('username', req.body.username);
+  res.redirect('/');
+});
+
+app.post('/goodbye', (req, res) => {
+  res.clearCookie('username');
+  res.redirect('/hello');
+});
