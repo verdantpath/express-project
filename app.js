9f9f553cfafb76846adc9a040204198c49b924b0
verdantpath <m_dittmann@yahoo.com>
2019-02-20 22:12:57 -0800

    move routes to index file

--- app.js
+++ app.js
@@ -16,39 +16,7 @@ app.set('view engine', 'pug');
 //   next(err);
 // });
 
-app.get('/', (req, res) => {
-  const name = req.cookies.username;
-  if (name) {
-    res.render('index', { name });
-  } else {
-    res.redirect('/hello');
-  }
-});
 
-app.get('/cards', (req, res) => {
-  res.render('card', {
-    prompt: "Who is buried in Grant's tomb?"
-  });
-});
-
-app.get('/hello', (req, res) => {
-  const name = req.cookies.username;
-  if (name) {
-    res.redirect('/');
-  } else {
-    res.render('hello');
-  }
-});
-
-app.post('/hello', (req, res) => {
-  res.cookie('username', req.body.username);
-  res.redirect('/');
-});
-
-app.post('/goodbye', (req, res) => {
-  res.clearCookie('username');
-  res.redirect('/hello');
-});
 
 app.use((req, res, next) => {
   const err = new Error('Not Found');
