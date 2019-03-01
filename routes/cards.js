b1d8e9fbbbf47ab3ca91cbf1d50723f0a660dda7
verdantpath <m_dittmann@yahoo.com>
2019-02-28 21:22:36 -0800

    modularize routes, parameters, query strings, linking around the app

--- routes/cards.js
+++ routes/cards.js
@@ -1,18 +1,36 @@
 const express = require('express');
 const router = express.Router();
+
 const {data} = require('../data/flashcardData.json');
 const {cards} = data;
 
+router.get('/', (req, res) => {
+  const numberOfCards = cards.length;
+  const flashCardId = Math.floor(Math.random() * numberOfCards);
+  res.redirect( `/cards/${flashCardId}` );
+});
+
 router.get('/:id', (req, res) => {
+  const name = req.cookies.username;
   const { side } = req.query;
-  const { id } = req.params;
+  const { id } = req.params
+
+  if(!side) {
+    res.redirect( `/cards/${id}?side=question` );
+  }
+
   const text = cards[id][side];
   const { hint } = cards[id];
-  const templateData = { text };
+  const templateData = { id, text, name };
 
   if(side === 'question') {
     templateData.hint = hint;
-  };
+    templateData.sideToShow = 'answer';
+    templateData.sideToShowDisplay = 'Answer';
+  } else if(side === 'answer') {
+    templateData.sideToShow = 'question';
+    templateData.sideToShowDisplay = 'Question';
+  }
   res.render('card', templateData);
 });
 
